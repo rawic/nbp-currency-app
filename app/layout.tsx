dda61ab38e30e4b67203b694714e4bafa2499e25
Rafał Wichowski <rawic.me@gmail.com>
2024-09-22 23:10:50 +0200

    cleanup: background image

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} relative flex min-h-full flex-col overflow-y-scroll bg-slate-900 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-slate-900 via-slate-900 to-slate-950 antialiased`}
       >
-        <div className="absolute -top-4 -z-10 h-[234px] w-full bg-[url('/bg2.svg')] bg-repeat-x" />
+        <div className="absolute -top-4 -z-10 h-[234px] w-full bg-[url('/bg.svg')] bg-repeat-x" />
         <Navigation />
         <div className="container mx-auto flex flex-col px-4 py-20 sm:py-28">
           <div className="rounded-xl border border-slate-800 p-4 shadow-sm backdrop-blur-sm sm:p-6">
